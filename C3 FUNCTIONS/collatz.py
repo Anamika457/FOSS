85988d3fb13e4579ca327e82fb05720058254ce7
Anamika V Menon <anamikavmenon1@gmail.com>
2023-11-16 18:47:29 +0530

    adding solutions

---
+++ C3 FUNCTIONS/collatz.py
@@ -0,0 +1,11 @@
+def collatz(number):
+    if number%2 ==0:
+        result= number//2
+    else:
+        result= 3*number+1
+    print(result)
+    return result
+
+num=int(input("Enter a number:"))
+while num!= 1:
+    num=collatz(num)
